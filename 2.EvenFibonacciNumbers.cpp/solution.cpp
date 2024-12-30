17b37499e7b3bbfabfe3fc7fc5e763bdb7c528c3
peperoni <brucewayne0002@proton.me>
2024-12-30 16:14:31 +0000

    solved

--- 2.EvenFibonacciNumbers.cpp/solution.cpp
+++ 2.EvenFibonacciNumbers.cpp/solution.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <numeric>
+#include <vector>
 
 int fib(int n) {
-  if (n <= 1) {
-    return n; // Base cases: fib(0) = 0, fib(1) = 1
-  }
-  return fib(n - 1) + fib(n - 2); // Recursive case
+	if (n <= 1) {
+		return n; // Base cases: fib(0) = 0, fib(1) = 1
+	}
+	return fib(n - 1) + fib(n - 2); // Recursive case
 }
 
 int main() {
-	std::cout << fib(10);
+	std::vector<int> even;
+	for (int i = 1; i<=4000000; i++) {
+		int n = fib(i);
+		if (n > 4000000) {break;}
+		if (n % 2 == 0) {
+			even.push_back(n);
+		}
+	}
+	std::cout << std::accumulate(even.begin(),even.end(),0);
 	return 0;
 }
 
